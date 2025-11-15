0d6ec31dc4f2cc1bad037c0b3ad269f3d682f2f4
Ufuoma Gideon Iboyi <u.iboyi@heunets.com>
2025-11-15 15:54:35 +0100

    fix to  user schema

--- models/User.js
+++ models/User.js
@@ -3,17 +3,17 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  phone: { type: String, unique: true, trim: true },  // remove required
+  phone: { type: String, unique: true, sparse: true, trim: true },
   password: { type: String },  
-  role: { type: String, enum: ['recruiter','trainer'], required: true },
-  googleId: { type: String },   
+  role: { type: String, enum: ['recruiter', 'trainer'], required: true },
+  googleId: { type: String, sparse: true },   
   createdAt: { type: Date, default: Date.now }
 });
 
-
-// hash password before save
 UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+  if (!this.password) return next();               
+  if (!this.isModified('password')) return next(); 
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
